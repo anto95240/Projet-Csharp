241ae8ec11e6d368c7c154a28a97efa14b7c2313
vyvenlee <yvenlee.voninkabel@ynov.com>
2023-12-05 17:02:36 +0100

    lesClassesDesTables

---
+++ Projet-Csharp/LePetitDeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Projet_Csharp
+{
+    public class YourDbContext : DbContext
+    {
+        public DbSet<user_Table> Users { get; set; }
+        public DbSet<address_Table> Addresses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Configurez la connexion à votre base de données ici
+            optionsBuilder.UseSqlServer("e-commerce.mdf");
+        }
+    }
+}
---
+++ Projet-Csharp/address_Table.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projet_Csharp
+{
+    public class address_Table
+    {
+        [Key]
+        public int AddressId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public int PostalCode { get; set; }
+
+        public string Country { get; set; }
+
+    }
+}
